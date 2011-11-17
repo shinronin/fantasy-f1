5500d42130282fccac75b8014d72fbb6750a963e
Sean O'Donnell <sean.reads@gmail.com>
2011-11-17 13:44:00 -0500

    init rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiordanFantasyF1::Application.config.secret_token = '450d883d73de845a7ab2d0f8eb43c1bd9c9ecfe753014310d98295617bbe49fc0efa9f3b812dd3881e6cea61f6109402695f58cbbb54aca4f3b420c9b0c80531'
